5d9027459d03edff3b786f14a23f16c7d5c99f5f
Mohammed Hashim Nilger <hashimrangrezz786@gmail.com>
2026-08-20 13:27:34 +0530

    Time: 0 ms (100%), Space: 97.2 MB (99.51%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,16 +3,9 @@ public:
     int maxProfit(vector<int>& prices) {
         int minPrice = prices[0];
         int maxProfit = 0;
-        for(int i = 1; i < prices.size(); i++) {
-            int currentPrice = prices[i];
-            if(currentPrice < minPrice) {
-                minPrice = currentPrice;
-            }
-            int currentProfit = prices[i] - minPrice;
-
-            if(currentProfit > maxProfit) {
-                maxProfit = currentProfit;
-            }
+        for(int price : prices) {
+            minPrice = min(minPrice, price);
+            maxProfit = max(maxProfit, price-minPrice);
         }
         return maxProfit;
     }
