9bd18f35068c1b0b0731023d074a873af7ab3e53
Mohammed Hashim Nilger <hashimrangrezz786@gmail.com>
2026-08-22 13:22:53 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,9 +3,9 @@ public:
     void moveZeroes(vector<int>& nums) {
         int nonZeroIndex = 0;
         for(int i = 0; i < nums.size(); i++) {
-            int temp = nums[i];
+            
             if(nums[i] != 0) {
-                temp = nums[nonZeroIndex];
+                int temp = nums[nonZeroIndex];
                 nums[nonZeroIndex] = nums[i]; 
                 nums[i] = temp;
 
