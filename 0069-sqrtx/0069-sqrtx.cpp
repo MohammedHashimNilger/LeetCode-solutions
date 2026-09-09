2178381497262d14595871f4fa1eeb4280307b5f
Mohammed Hashim Nilger <hashimrangrezz786@gmail.com>
2026-09-09 16:40:32 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.16%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    long long int binarySearch(int n) {
+        int s = 0;
+        int e = n;
+        int ans = -1;
+
+        while(s<=e) {
+           long long int mid = s+(e-s)/2;
+           long long int sqr = mid*mid;
+
+            if(sqr == n)
+                return mid;
+            if(sqr < n) {
+                ans = mid;
+                s = mid+1;
+            } else {
+                e = mid-1;
+            }
+
+        }
+        return ans;
+    }
+
+    int mySqrt(int x) {
+       return binarySearch(x);
+    }
+};
