7c4a14353301855603a3395b8c686837f0ffe325
Mohammed Hashim Nilger <hashimrangrezz786@gmail.com>
2026-09-14 15:35:48 +0530

    Time: 0 ms (100%), Space: 10.7 MB (74.01%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
         int highest = 0;
+        int alt = 0;
         for(int i = 0; i<gain.size(); i++) {
-            int alt = gain[i] + alt;
+            alt = gain[i] + alt;
             if(alt > highest) {
                 highest = alt;
             }
